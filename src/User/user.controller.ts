4fcd19d0cd0521602a6d16bbf80c16ed37a88f0c
tanmayrathod777 <tanmay.rathod@digiqt.com>
2023-03-06 17:25:27 +0530

    folder_structure

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from '../dto/create.user.dto';
+import { CreateUserDto } from './dto/create.user.dto'
+import { UpdateUserDto } from './dto/update.user.dto';
 
-@Controller('users')
+@Controller('api')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/v1/user')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Get('/v1/user')
+  async findAll() {
+    return this.userService.getUsers();
+  }
+
+  @Get('/v1/user/:id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.getUserById(id);
+  }
+
+  @Put('/v1/user/:id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete('/v1/user/:id')
+  async delete(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './User/user.controller';
+import { UserService } from './User/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './User/user.schema';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/nutrafusion-backend'), 
             MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
