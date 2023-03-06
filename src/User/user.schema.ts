9baf97e8fb9356bc7e5448eeda6d01d8b1f64244
tanmayrathod777 <tanmay.rathod@digiqt.com>
2023-03-06 18:02:00 +0530

    new_error

--- src/User/user.schema.ts
+++ src/User/user.schema.ts
@@ -3,9 +3,18 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
   id: String,
   name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
   password: String,
-  gender: String,
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female', 'other'],
+  },
 }, {
   timestamps: true,
 });
--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.interface';
@@ -10,8 +10,16 @@ export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
+    try {
+      const createdUser = new this.userModel(createUserDto);
+      return await createdUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new HttpException('Email address already in use', HttpStatus.CONFLICT);
+      } else {
+        throw new HttpException('Failed to create user', HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
   }
 
   async getUsers(): Promise<User[]> {
@@ -19,14 +27,38 @@ export class UserService {
   }
 
   async getUserById(id: string): Promise<User> {
-    return this.userModel.findById(id).exec();
+    try {
+      const user = await this.userModel.findById(id).exec();
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException('Failed to get user', HttpStatus.NOT_FOUND);
+    }
   }
 
   async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+    try {
+      const updatedUser = await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+      if (!updatedUser) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      return updatedUser;
+    } catch (error) {
+      throw new HttpException('Failed to update user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async deleteUser(id: string): Promise<User> {
-    return this.userModel.findByIdAndRemove(id).exec();
+    try {
+      const deletedUser = await this.userModel.findByIdAndRemove(id).exec();
+      if (!deletedUser) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      return deletedUser;
+    } catch (error) {
+      throw new HttpException('Failed to delete user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
