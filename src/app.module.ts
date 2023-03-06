8652c35a4ac8d76b415c35aaff3485c72a5f406f
tanmayrathod777 <tanmay.rathod@digiqt.com>
2023-03-06 15:40:13 +0530

    new_db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './User/user.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), 
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nutrafusion-backend'), 
             MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [AppController],
   providers: [AppService],
